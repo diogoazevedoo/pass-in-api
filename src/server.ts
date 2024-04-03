4f66c1e8d41057122c4d0558fb2c4baa22ebfda8
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-04-03 23:34:50 +0100

    Route: create route to create events, installed zod for data validation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,33 @@
 import fastify from "fastify"
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client"
 
 const app = fastify()
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+app.post('/events', async (request, reply) => {
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        description: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.description,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        }
+    })
+
+    return reply.status(201).send({ eventId: event.id })
+})
 
 app.listen({ port: 3333 }).then(() => {
     console.log("🔥 HTTP server running on port :3333")
